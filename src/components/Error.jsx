bf2663fac8d62e64ede873c0ddcb811c4bf24c8f
Abdulrahman Berro <72647782+abdulrahmanberro@users.noreply.github.com>
2022-09-28 00:16:18 +0300

    .

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import limit from "../assets/limit.png";
 
 const Error = () => (
   <div className="w-full flex justify-center items-center">
     <h1 className="font-bold text-2xl text-white text-center mt-2">
       Something Went Wrong Please Try Again.
+      <span className="font-normal mt-3 block">
+        If You Keep Seeing This Message Then I'm So Sorry This Is Because I'm
+        using a Free Api That Only Allows Me To Send 500 Api Calls a Month.
+      </span>
+      <div className="flex mt-5 justify-center shadow-slate-700 shadow-2xl">
+        <img src={limit} alt="" />
+      </div>
     </h1>
   </div>
 );
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -6,24 +5,27 @@ import { Error, Loader, SongCard } from "../components";
 import { useGetSongsBySearchQuery } from "../redux/services/shazamCore";
 
 const Search = () => {
-  const { searchTerm } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetSongsBySearchQuery(searchTerm);
+  const { searhTerm } = useParams();
+  const { data, isFetching, error } = useGetSongsBySearchQuery(searhTerm);
 
-  const songs = data?.tracks?.hits.map((song) => song.track);
+  const songs = data?.tracks?.hits?.map((song) => song.track);
 
-  if (isFetching) return <Loader title={`Searching ${searchTerm}...`} />;
+  if (isFetching) {
+    return <Loader title="Loading Songs Around You" />;
+  }
 
-  if (error) return <Error />;
+  if (error) {
+    return <Error />;
+  }
 
   return (
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Showing results for <span className="font-black">{searchTerm}</span>
+        Search Results for <span className="font-black">{searhTerm}</span>
       </h2>
-
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {songs.map((song, i) => (
+        {songs?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
